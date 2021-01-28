65270a87dc68bc89a86cdb62aeec4b7ab3a000be
chuddyjoachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-01-27 16:12:20 -0800

    added login and delete users route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { auth } from "../middleware/auth";
+
 const router = Router();
 
 import {
   createUsers,
   getUsers,
   loginUser,
+  deleteUser
 } from "../controllers/user.controller";
 
 (async () => {
   router.get("/", getUsers);
   router.post("/register", createUsers);
   router.post("/login", loginUser);
+  router.delete("/delete", auth, deleteUser);
 })();
 
 export default router;
