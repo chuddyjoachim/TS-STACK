d88ddd38d67279058e64afe08eaec544abb334a2
chuddyjoachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-02-01 14:08:49 -0800

    added token generator functions

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,9 @@
 import * as jwt from "jsonwebtoken";
 import * as express from "express";
+import { User } from "../entity/User";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const auth = (
   req: express.Request,
@@ -31,4 +35,24 @@ export const auth = (
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+};
+
+export const createAccessToken = (user: User) => {
+  return jwt.sign(
+    { userId: user.id },
+    process.env.JWT_SECRET ? process.env.JWT_SECRET : "",
+    {
+      expiresIn: "15m"
+    }
+  );
+};
+
+export const refreshToken = (user: User) => {
+  return jwt.sign(
+    { userId: user.id, tokenVersion: user.tokenVersion },
+    process.env.REFRESH_TOKEN ? process.env.REFRESH_TOKEN : "",
+    {
+      expiresIn: "7d"
+    }
+  );
 };
