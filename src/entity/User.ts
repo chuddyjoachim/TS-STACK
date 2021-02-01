24d34dc09e00e2704e3198d28fb1a18ef9d1ddf1
chuddyjoachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-02-01 14:13:12 -0800

    Apolloqraphql server+jsonwebtoken graphql api auth

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,22 +1,32 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
 
+@ObjectType()
 @Entity({ name: "user" })
-export class User {
+export class User extends BaseEntity {
+  @Field(() => String)
   @PrimaryGeneratedColumn("uuid")
-  id: number;
+  id: string;
 
+  @Field()
   @Column()
   firstname: string;
 
+  @Field()
   @Column()
   lastname: string;
 
-  @Column()
+  @Field()
+  @Column("text")
   email: string;
 
+  @Field()
   @Column()
   username: string;
 
-  @Column()
+  @Column("text")
   password: string;
+
+  @Column("int", { default: 0 })
+  tokenVersion: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server-express";
 
 import userRoutes from "./routes/user.routes";
+import { buildSchema } from "type-graphql";
+import { userResolver } from "./resolvers/userResolver";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken, refreshToken } from "./middleware/auth";
 
 dotenv.config();
 const App_Port = process.env.App_Port;
@@ -50,6 +57,46 @@ let retries = 5;
 
   // other logic \\
   const serverFunc = async () => {
+    app.use(cookieParser());
     app.use("/users", userRoutes);
+
+    app.post("/refresh_token", async (req, res) => {
+      const token = req.cookies.rfx;
+      if (!token) {
+        return res.send({ ok: false, accessToken: "" });
+      }
+
+      let payload: any = null;
+      try {
+        payload = verify(token, process.env.REFRESH_TOKEN!);
+      } catch (err) {
+        console.log(err);
+        return res.send({ ok: false, accessToken: "" });
+      }
+
+      const user = await User.findOne({ id: payload.userId });
+      if (!user) {
+        return res.send({ ok: false, accessToken: "" });
+      }
+
+      if (user.tokenVersion !== payload.tokenVersion) {
+        return res.send({ ok: false, accessToken: "" });
+      }
+
+      res.cookie("rfx", refreshToken(user), {
+        httpOnly: true
+      });
+      return res.send({ ok: true, accessToken: createAccessToken(user) });
+    });
+
+    // apolloserver graphql
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [userResolver]
+      }),
+      context: ({ req, res }) => ({ req, res })
+    });
+
+    apolloServer.applyMiddleware({ app });
   };
 })();
