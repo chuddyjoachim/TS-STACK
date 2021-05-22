bf4bc89da1e88cbf12d1b2db5be4e10539af0215
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-05-21 22:13:13 -0700

    Test graphql response

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
+import { useQuery } from "@apollo/client";
+import { gql } from "apollo-boost";
+import React from "react";
 import "./App.css";
 
 function App() {
+  const { data, loading } = useQuery(
+    gql`
+      {
+        hello
+      }
+    `
+  );
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>hello </h1>
+      <h4>{JSON.stringify(data)}</h4>
     </div>
   );
 }
