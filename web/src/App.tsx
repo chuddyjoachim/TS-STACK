90204fea9cf299e05a9069ae3bce85241de0069e
joachim chikezie <56943504+chuddyjoachim@users.noreply.github.com>
2021-05-26 10:35:01 -0700

    Added Routes component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,10 @@
-import { useQuery } from "@apollo/client";
-import { gql } from "apollo-boost";
 import React from "react";
-import "./App.css";
+import Routes from "./Routes";
 
-function App() {
-  const { data, loading } = useQuery(
-    gql`
-      {
-        hello
-      }
-    `
-  );
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
+interface AppProps {}
 
-  return (
-    <div className="">
-      <h1>hello </h1>
-      <h4>{JSON.stringify(data)}</h4>
-    </div>
-  );
-}
+const App: React.FC<AppProps> = () => {
+  return <Routes />;
+};
 
 export { App };
