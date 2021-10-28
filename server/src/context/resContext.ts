8ac8fa9a0f0ec64cb46cbe3255d8648fc1cc7b84
Chikezie Joachim <56943504+chuddyjoachim@users.noreply.github.com>
2021-10-28 09:25:48 +0100

    Add usserId to sesscion context

--- server/src/context/resContext.ts
+++ server/src/context/resContext.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import { Session } from "express-session";
 
 export interface resContext {
-  req: Request;
+  req: Request & { session?: Session & { userId?: string } }; //& { session?: Express.Session};
   res: Response;
   payload?: { userId: string };
 }
